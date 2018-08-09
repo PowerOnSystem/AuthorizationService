4d06c1f08c5e6b9eace80dbe7e28c33ce804b443
Lucas Sosa <sosalucas87@gmail.com>
2018-08-09 15:45:49 -0300

    Bye servicio de traducción, now you're free

--- langs/authorization.es.php
+++ langs/authorization.es.php
@@ -1,13 +1,3 @@
 <?php
 
-return [
-    'login_error_max_chances' => '',
-    'invalid_credentials' => 'Los datos de las credenciales no son correctos.',
-    'user_not_found' => '',
-    'banned_account' => '',
-    'sector_require_login' => 'Se requiere inicio de sesi&oacute;n para continuar.',
-    'sector_require_access' => 'No tiene autorizaci&oacute;n de ingreso a este sector.',
-    'sector_ajax_error' => 'Este sector no admite peticiones Ajax.',
-    'sector_ajax_required' => 'Este sector solo puede ser accedido por una petici&oacute;n Ajax.',
-    'sector_ajax_post' => 'Este sector solo recibe datos de peticiones Ajax.',
-];
+return [];
